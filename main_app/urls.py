6218dfbebf35991e867eb3c3856e00128bae50c1
Alexei Udarbe <alexeiudarbe@AU-MacBook-Bro.local>
2020-06-17 16:04:17 -0700

    CBV: List/Detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import PopList
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('pops/', views.pops_index, name='index'),
-    path('pops/<int:pop_id>', views.pops_detail, name='detail'),
+    path('pops/', views.PopList.as_view(), name='pops_index'),
+    path('pops/<int:pk>/', views.PopDetail.as_view(), name='pops_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Pop
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 def home(request):
@@ -8,10 +10,11 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def pops_index(request):
-    pops = Pop.objects.all()
-    return render(request, 'pops/index.html', {'pops': pops})
+class PopList(ListView):
+    model = Pop
+    template_name = 'pops/index.html'
 
-def pops_detail(request, pop_id):
-    pop = Pop.objects.get(id=pop_id)
-    return render(request, 'pops/detail.html', {'pop' : pop})
+class PopDetail(DetailView):
+    model = Pop
+    template_name = 'pops/detail.html'
+
